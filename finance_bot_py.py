9cf5aa42b6ec5ed9919e3a7b8cc069a41db825b5
Hochheilige <hochheilige01@gmail.com>
2026-05-11 17:05:13 +0800

    fix formulas

--- finance_bot_py.py
+++ finance_bot_py.py
@@ -71,7 +71,8 @@ def get_or_create_user_sheet(name):
         ws = work_sheet.add_worksheet(title=name, rows=1010, cols=5)
         ws.update('A1:E1', [['date', 'category', 'amount (RM)',
                               '=SUM(C2:C1000)',
-                              '=D1*GOOGLEFINANCE("CURRENCY:MYRUB")']])
+                              '=D1*GOOGLEFINANCE("CURRENCY:MYRUB")']],
+                  value_input_option='USER_ENTERED')
         return ws
 
 
